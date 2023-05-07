025c4397ba156694ac8e85713d68e359852773ec
jaeho <jaeho@plrg2.kaist.ac.kr>
2023-05-07 15:54:41 +0900

    Add ReLU import

--- tools/training/architectures.py
+++ tools/training/architectures.py
@@ -6,52 +6,55 @@ from gloro.layers import Input
 from gloro.layers import InvertibleDownsampling
 from gloro.layers import LiResNetBlock
 from gloro.layers import MaxPooling2D
-from gloro.layers import MinMax
+from gloro.layers import MinMax, ReLU
 
 
-def Activation(activation_type='minmax'):
-    if activation_type == 'minmax':
+def Activation(activation_type="minmax"):
+    if activation_type == "minmax":
         return MinMax()
 
-    if activation_type == 'relu':
+    if activation_type == "relu":
         return ReLU()
 
     else:
-        raise ValueError(f'unknown activation type: {activation_type}')
+        raise ValueError(f"unknown activation type: {activation_type}")
+
 
 def Downsample(downsample_type):
-    if downsample_type == 'invertible':
+    if downsample_type == "invertible":
         return InvertibleDownsampling(2)
 
-    elif downsample_type == 'avg':
+    elif downsample_type == "avg":
         return AveragePooling2D(2)
 
-    elif downsample_type == 'max':
+    elif downsample_type == "max":
         return MaxPooling2D(2)
 
     else:
-        raise ValueError(f'unknown downsample type: {downsample_type}')
+        raise ValueError(f"unknown downsample type: {downsample_type}")
 
 
 def cnn_2C2F(
-    input_shape, 
-    num_classes, 
-    activation='minmax',
-    initialization='orthogonal',
+    input_shape,
+    num_classes,
+    activation="minmax",
+    initialization="orthogonal",
 ):
     x = Input(input_shape)
     z = Conv2D(
-        16, 4,
+        16,
+        4,
         strides=2,
-        padding='same',
+        padding="same",
         kernel_initializer=initialization,
     )(x)
     z = Activation(activation)(z)
 
     z = Conv2D(
-        32, 4,
+        32,
+        4,
         strides=2,
-        padding='same',
+        padding="same",
         kernel_initializer=initialization,
     )(z)
     z = Activation(activation)(z)
@@ -66,30 +69,32 @@ def cnn_2C2F(
 
 
 def cnn_4C3F(
-    input_shape, 
-    num_classes, 
-    pooling='conv', 
-    activation='minmax',
-    initialization='orthogonal',
+    input_shape,
+    num_classes,
+    pooling="conv",
+    activation="minmax",
+    initialization="orthogonal",
 ):
     x = Input(input_shape)
 
-    z = Conv2D(32, 3, padding='same', kernel_initializer=initialization)(x)
+    z = Conv2D(32, 3, padding="same", kernel_initializer=initialization)(x)
     z = Activation(activation)(z)
     z = Conv2D(
-        32, 4,
+        32,
+        4,
         strides=2,
-        padding='same',
+        padding="same",
         kernel_initializer=initialization,
     )(z)
     z = Activation(activation)(z)
 
-    z = Conv2D(64, 3, padding='same', kernel_initializer=initialization)(z)
+    z = Conv2D(64, 3, padding="same", kernel_initializer=initialization)(z)
     z = Activation(activation)(z)
     z = Conv2D(
-        64, 4,
+        64,
+        4,
         strides=2,
-        padding='same',
+        padding="same",
         kernel_initializer=initialization,
     )(z)
     z = Activation(activation)(z)
@@ -106,33 +111,35 @@ def cnn_4C3F(
 
 
 def cnn_6C2F(
-    input_shape, 
-    num_classes, 
-    activation='minmax',
-    initialization='orthogonal',
+    input_shape,
+    num_classes,
+    activation="minmax",
+    initialization="orthogonal",
 ):
     x = Input(input_shape)
 
-    z = Conv2D(32, 3, padding='same', kernel_initializer=initialization)(x)
+    z = Conv2D(32, 3, padding="same", kernel_initializer=initialization)(x)
     z = Activation(activation)(z)
-    z = Conv2D(32, 3, padding='same', kernel_initializer=initialization)(z)
+    z = Conv2D(32, 3, padding="same", kernel_initializer=initialization)(z)
     z = Activation(activation)(z)
     z = Conv2D(
-        32, 4,
+        32,
+        4,
         strides=2,
-        padding='same',
+        padding="same",
         kernel_initializer=initialization,
     )(z)
     z = Activation(activation)(z)
 
-    z = Conv2D(64, 3, padding='same', kernel_initializer=initialization)(z)
+    z = Conv2D(64, 3, padding="same", kernel_initializer=initialization)(z)
     z = Activation(activation)(z)
-    z = Conv2D(64, 3, padding='same', kernel_initializer=initialization)(z)
+    z = Conv2D(64, 3, padding="same", kernel_initializer=initialization)(z)
     z = Activation(activation)(z)
     z = Conv2D(
-        64, 4,
+        64,
+        4,
         strides=2,
-        padding='same',
+        padding="same",
         kernel_initializer=initialization,
     )(z)
     z = Activation(activation)(z)
@@ -147,43 +154,46 @@ def cnn_6C2F(
 
 
 def cnn_8C2F(
-    input_shape, 
-    num_classes, 
-    activation='minmax',
-    initialization='orthogonal',
+    input_shape,
+    num_classes,
+    activation="minmax",
+    initialization="orthogonal",
 ):
     x = Input(input_shape)
 
-    z = Conv2D(64, 3, padding='same', kernel_initializer=initialization)(x)
+    z = Conv2D(64, 3, padding="same", kernel_initializer=initialization)(x)
     z = Activation(activation)(z)
-    z = Conv2D(64, 3, padding='same', kernel_initializer=initialization)(z)
+    z = Conv2D(64, 3, padding="same", kernel_initializer=initialization)(z)
     z = Activation(activation)(z)
     z = Conv2D(
-        64, 4,
+        64,
+        4,
         strides=2,
-        padding='same',
+        padding="same",
         kernel_initializer=initialization,
     )(z)
     z = Activation(activation)(z)
 
-    z = Conv2D(128, 3, padding='same', kernel_initializer=initialization)(z)
+    z = Conv2D(128, 3, padding="same", kernel_initializer=initialization)(z)
     z = Activation(activation)(z)
-    z = Conv2D(128, 3, padding='same', kernel_initializer=initialization)(z)
+    z = Conv2D(128, 3, padding="same", kernel_initializer=initialization)(z)
     z = Activation(activation)(z)
     z = Conv2D(
-        128, 4,
+        128,
+        4,
         strides=2,
-        padding='same',
+        padding="same",
         kernel_initializer=initialization,
     )(z)
     z = Activation(activation)(z)
 
-    z = Conv2D(256, 3, padding='same', kernel_initializer=initialization)(z)
+    z = Conv2D(256, 3, padding="same", kernel_initializer=initialization)(z)
     z = Activation(activation)(z)
     z = Conv2D(
-        256, 4,
+        256,
+        4,
         strides=2,
-        padding='same',
+        padding="same",
         kernel_initializer=initialization,
     )(z)
     z = Activation(activation)(z)
@@ -202,17 +212,18 @@ def _cnn_CxCC2F(
     backbone_width,
     input_shape,
     num_classes,
-    activation='minmax',
-    initialization='orthogonal',
+    activation="minmax",
+    initialization="orthogonal",
     stem_downsample=2,
 ):
     x = Input(input_shape)
 
     # Stem.
     z = Conv2D(
-        backbone_width, 5,
+        backbone_width,
+        5,
         strides=stem_downsample,
-        padding='same',
+        padding="same",
         kernel_initializer=initialization,
     )(x)
     z = Activation(activation)(z)
@@ -220,17 +231,19 @@ def _cnn_CxCC2F(
     # Backbone.
     for _ in range(backbone_depth):
         z = Conv2D(
-            backbone_width, 3,
-            padding='same',
+            backbone_width,
+            3,
+            padding="same",
             kernel_initializer=initialization,
         )(z)
         z = Activation(activation)(z)
 
     # Neck.
     z = Conv2D(
-        2 * backbone_width, 4,
+        2 * backbone_width,
+        4,
         strides=4,
-        padding='valid',
+        padding="valid",
         kernel_initializer=initialization,
     )(z)
     z = Activation(activation)(z)
@@ -244,16 +257,20 @@ def _cnn_CxCC2F(
 
     return x, y
 
+
 def cnn_C6CC2F(
     input_shape,
     num_classes,
     width=128,
-    activation='minmax',
-    initialization='orthogonal',
+    activation="minmax",
+    initialization="orthogonal",
     stem_downsample=2,
 ):
     return _cnn_CxCC2F(
-        6, width, input_shape, num_classes,
+        6,
+        width,
+        input_shape,
+        num_classes,
         activation=activation,
         initialization=initialization,
         stem_downsample=stem_downsample,
@@ -265,17 +282,18 @@ def _liresnet_CxCC2F(
     backbone_width,
     input_shape,
     num_classes,
-    activation='minmax',
-    initialization='orthogonal',
+    activation="minmax",
+    initialization="orthogonal",
     stem_downsample=2,
 ):
     x = Input(input_shape)
 
     # Stem.
     z = Conv2D(
-        backbone_width, 5,
+        backbone_width,
+        5,
         strides=stem_downsample,
-        padding='same',
+        padding="same",
         kernel_initializer=initialization,
     )(x)
     z = Activation(activation)(z)
@@ -284,16 +302,17 @@ def _liresnet_CxCC2F(
     for _ in range(backbone_depth):
         z = LiResNetBlock(
             3,
-            residual_scale=backbone_depth**(-0.5),
+            residual_scale=backbone_depth ** (-0.5),
             kernel_initializer=initialization,
         )(z)
         z = Activation(activation)(z)
 
     # Neck.
     z = Conv2D(
-        2 * backbone_width, 4,
+        2 * backbone_width,
+        4,
         strides=4,
-        padding='valid',
+        padding="valid",
         kernel_initializer=initialization,
     )(z)
     z = Activation(activation)(z)
@@ -307,33 +326,40 @@ def _liresnet_CxCC2F(
 
     return x, y
 
+
 def liresnet_C6CC2F(
     input_shape,
     num_classes,
     width=128,
-    activation='minmax',
-    initialization='orthogonal',
+    activation="minmax",
+    initialization="orthogonal",
     stem_downsample=2,
 ):
     return _liresnet_CxCC2F(
-        6, width, input_shape, num_classes,
+        6,
+        width,
+        input_shape,
+        num_classes,
         activation=activation,
         initialization=initialization,
         stem_downsample=stem_downsample,
     )
+
 
 def liresnet_C18CC2F(
     input_shape,
     num_classes,
     width=256,
-    activation='minmax',
-    initialization='orthogonal',
+    activation="minmax",
+    initialization="orthogonal",
     stem_downsample=2,
 ):
     return _liresnet_CxCC2F(
-        18, width, input_shape, num_classes,
+        18,
+        width,
+        input_shape,
+        num_classes,
         activation=activation,
         initialization=initialization,
         stem_downsample=stem_downsample,
     )
-
